d04992ef6b46cd18367be8798324ee62e91a732d
robertoahc85 <robertoahc85@gmail.com>
2024-07-31 08:50:40 -0600

    Agregando el archivo de variable en python

---
+++ variables.py
@@ -0,0 +1,24 @@
+#Variable tipo entero
+a=10
+b=20
+print(a) 
+print(b)
+#Variable flotantes
+c=1.5
+d=3.7
+suma = c + d
+print(suma)
+#variable tipo string
+nombre = "Roberto"
+apellido= 'Hernandez'
+fullName = nombre + " " + apellido
+print(fullName)
+# Variable tipo boolean
+lampara=True
+print(lampara)
+lampara=False
+print(lampara)
+
+
+
+
