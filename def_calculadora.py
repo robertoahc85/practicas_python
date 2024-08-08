806a9be434d263ed403aaf598e796d2d7f9918a8
robertoahc85 <robertoahc85@gmail.com>
2024-08-08 08:47:15 -0600

    Agregar calculadora

--- def_calculadora.py
+++ def_calculadora.py
@@ -7,6 +7,13 @@ def multiplicar(a,b):
     return a*b
 def dividir(a,b):
     return a/b
+def imprimir_resultado(a,b,resultado):
+    print(f"El Resulado de la Resta de: {a} y {b} es: {resultado}")
+    
+def solicitar_numeros(num1,num2):
+    num1=float(input("ingresa el primer numero: "))
+    num2=float(input("ingresa el segundo numero: "))
+    return num1,num2
 
 def calculadora():
     print("------Bienvenido a la Calculadora 0077------")
@@ -17,28 +24,28 @@ def calculadora():
         print("3.Multiplicar")
         print("4.Dividir")
         print("5.Salir")
-        opcion = input("Ingresa tu opcion del(1-5)") 
+        opcion = int(input("Ingresa tu opcion del(1-5)"))
         
-        if opcion ==1:
-            num1=float(input("ingresa el primer numero"))
-            num2=float(input("ingresa el segundo numero"))
+        if opcion == 1 :
+            num1=float(input("ingresa el primer numero: "))
+            num2=float(input("ingresa el segundo numero: "))
             resultado=sumar(num1,num2)
-            print(f"El Resulado de la Suma de: {num1} y {num2} es: {resultado}")
+            imprimir_resultado(num1,num2,resultado)
         elif opcion ==2:
             num1=float(input("ingresa el primer numero"))
             num2=float(input("ingresa el segundo numero"))
             resultado=resta(num1,num2)
-            print(f"El Resulado de la Resta de: {num1} y {num2} es: {resultado}")
+            imprimir_resultado(num1,num2,resultado)
         elif opcion ==3:
             num1=float(input("ingresa el primer numero"))
             num2=float(input("ingresa el segundo numero"))
             resultado=multiplicar(num1,num2)
-            print(f"El Resulado de la Multiplicacion de: {num1} y {num2} es: {resultado}") 
+            imprimir_resultado(num1,num2,resultado)
         elif opcion ==4:
             num1=float(input("ingresa el primer numero"))
             num2=float(input("ingresa el segundo numero"))
             resultado=dividir(num1,num2)
-            print(f"El Resulado de la Division de: {num1} y {num2} es: {resultado}")
+            imprimir_resultado(num1,num2,resultado)
         elif opcion == 5:  
             print("Hasta la proxima")  
             break  
